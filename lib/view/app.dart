6568b528494e08de6795bbed7f4a45db87f4622b
arthur <arthurjbeluci@gmail.com>
2022-05-03 14:03:05 -0300

    teste

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'login.view.dart';
 
-
 class App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: LoginView(),
+      //alteração
     );
   }
 }
