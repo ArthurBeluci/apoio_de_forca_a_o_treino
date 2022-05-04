64cfc157f3bbbdd7d506396bdade0c21cb3ba639
Diego Armindo <diego.armindo@newm.com.br>
2022-05-03 22:11:27 -0300

    Nome certo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_application_1
+# apoio_forca_treino
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_application_1
+package com.example.apoio_forca_treino
 
 import io.flutter.embedding.android.FlutterActivity
 
