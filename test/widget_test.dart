ea73864f07138ba175d8c7a4a230ceadc5ab999f
Diego Armindo <diego.armindo@newm.com.br>
2022-05-03 21:54:58 -0300

    Alteração arquivos do projeto

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_1/main.dart';
+import 'package:apoio_forca_treino/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
